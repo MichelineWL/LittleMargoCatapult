0720a907fd5704b85c211c0b3131997446ee317d
MichelineWL <michelinewijaya@gmail.com>
2025-07-28 15:15:01 +0700

    refactor: katalog page mobile version

--- app/catalogue/page.tsx
+++ app/catalogue/page.tsx
@@ -3,8 +3,8 @@ import Flipbook from "./components/Flipbook";
 
 export default function cataloguePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-min-screen">
-      <h1 className="text-4xl font-bold my-10">Catalogue Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-8 px-4">
+      <h1 className="text-2xl md:text-4xl font-bold my-6 md:my-10 text-center">Catalogue Page</h1>
       <Flipbook />
     </div>
   );
